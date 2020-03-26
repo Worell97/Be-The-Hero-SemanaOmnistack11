8867a7b109607feed409df1c015d5b1ae78904ec
Oscar W. Filho <oscar.filho@eco.centauro>
2020-03-26 13:33:57 -0200

    Back-end

---
+++ BackEnd/src/routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+const routes = express.Router();
+
+
+routes.get('/login', SessionController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+routes.get('/profile', ProfileController.index);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+module.exports = routes;
+
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar/listar informações do back-end
+ * POST: Criando informações no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parametros nomeados enviados na rota após "?"(Filtros, Paginação)
+ * Route Params: Parametros utilizados para indentificar recursos
+ * Request Body: Corpo da requisição utilizado para criar ou alterar recursos
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
